b6f3f4ff3ddc42681641e0c0ea466dde5772137d
Gui Hebling <guilherme.durso@outlook.com>
2021-01-05 20:16:55 +0100

    Add globalStyles file

---
+++ src/theme/globalStyles.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+
+const GlobalStyle = createGlobalStyle`
+body {
+    font-family: -apple-system, Roboto, sans-serif, serif;
+}
+`
+
+export default GlobalStyle
